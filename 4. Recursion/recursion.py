ca18e2b88fcbf60f5ae3ba062d1315e61b2154ca
Gurleen Sethi <sarusethi@rocketmail.com>
2020-05-20 00:30:10 -0400

    reverse string and palindrome check with recursion.

--- 4. Recursion/recursion.py
+++ 4. Recursion/recursion.py
@@ -8,3 +8,17 @@ def factorial(n):
     if n == 0:
         return 1
     return n * factorial(n - 1)
+
+
+def reverse_string(input):
+    if not input:
+        return ""
+    return reverse_string(input[1:]) + input[0]
+
+
+def is_palindrome(input):
+    if len(input) == 0 or len(input) == 1:
+        return True
+    first_char = input[0]
+    last_char = input[-1]
+    return first_char == last_char and is_palindrome(input[1:-1])
