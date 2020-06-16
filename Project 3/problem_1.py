fae23bd694fb7dfc2f3bb78e3f918ba7327376c0
Gurleen Sethi <sarusethi@rocketmail.com>
2020-06-16 00:27:45 -0400

    Add documentation for square.

--- Project 3/problem_1.py
+++ Project 3/problem_1.py
@@ -1,3 +1,14 @@
+# My first approach was to start a counter set to zero 0,
+# increment it and check if sqaure of counter is equal to number or not.
+# While it did work, it didn't had time complexity if O(log(N)).
+#
+# To gain the time complexity of O(log(N)), I used binary search.
+# Do a binary search with start equal to 1 and end equal to the number.
+# Calculate to the square of mid and check if it is equal to the number,
+# if the square is less than number, move start to mid (as we need a larger number),
+# if it is greater, move the end to mid (as we need a smaller number).
+# Binary search has a time complexity of O(log(N)).
+
 def sqrt(number):
   # Base case
   if number == 0 or number == 1:
@@ -6,6 +17,7 @@ def sqrt(number):
   begin = 1
   end = number
   
+  # Binary search for the value
   while begin <= end:
     mid = (begin + end) // 2
   
