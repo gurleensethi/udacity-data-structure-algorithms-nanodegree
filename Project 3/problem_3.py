af1d28522f298673f246a700d96bb9b80306a7f7
Gurleen Sethi <sarusethi@rocketmail.com>
2020-06-16 02:41:55 -0400

    Explanation for rearrange elements.

--- Project 3/problem_3.py
+++ Project 3/problem_3.py
@@ -1,3 +1,18 @@
+# The solution of problem is straight forward, if we arrange the
+# elements in a sorted order and pick ever other element for num1 
+# and remaining element for num2 we have two numbers that have
+# maximum sum.
+#
+# Since I not allowed to use internal sort function, I have implemented
+# merge sort to sort the required arrays. Merge sort has a time complexity of O(N log(N)).
+# Once the array is sorted we iterate over the sorted array to form the numbers. This takes
+# O(N) time.
+#
+# Time complexity is O(N log(N) + N) which can be written as O(N log(N)).
+#
+# The space complexity is O(N) as the space complexity of merge sort is O(N), also we have to
+# store the sorted array.
+
 def merge_sort(input_list, start, end):
   if start == end:
     return [input_list[start]]
