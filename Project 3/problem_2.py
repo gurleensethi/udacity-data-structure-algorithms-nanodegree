4b22ec8c22be60cdb00bacd1feac695a2a81f17b
Gurleen Sethi <sarusethi@rocketmail.com>
2020-06-16 01:53:39 -0400

    Explanation for rotated sorted array search.

--- Project 3/problem_2.py
+++ Project 3/problem_2.py
@@ -1,3 +1,19 @@
+# I have used binary search to solve this problem.
+# Initially I tried modifying binary search's selection criteria, but it didn't work.
+# 
+# The problem is easy to solve if we run binary search by breaking the array into,
+# two parts from the point where it is rotated.
+#
+# Finding the rotation point by linear search would result in O(N) time.
+# I used binary search itself to search the rotation point in the array, 
+# thus taking O(log(N)) time to search.
+#
+# Once the rotation point has been found we can use binary search on both
+# the halfs of the array. This will take O(log(N)) for each half.
+#
+# The total time complexity is O(log(N)) + O(log(N)) + O(log(N)) = O(3log(N))
+# we drop the constant when N is very large so the complexity is O(log(N)).
+
 def simple_binary_search(input_list, start, end, number):
 
   while start <= end:
@@ -33,6 +49,7 @@ def rotated_array_search(input_list, number):
       else:
         start = mid + 1
 
+    # Array has been rotated
     if rotation_point != -1:
       first_half_result = simple_binary_search(input_list, 0, rotation_point, number)
       second_half_result = simple_binary_search(input_list, rotation_point + 1, len(input_list) - 1, number)      
