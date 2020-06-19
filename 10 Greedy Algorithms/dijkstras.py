ef767fe9598924e22d08f0883db83bfc48beed47
Gurleen Sethi <sarusethi@rocketmail.com>
2020-06-18 21:06:23 -0400

    Simple graph.

---
+++ 10 Greedy Algorithms/dijkstras.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+class Graph:
+  def __init__(self):
+    self.nodes = set()
+    self.neighbours = defaultdict(list)
+    self.distances = {}
+
+  def add_node(self, value):
+    self.nodes.add(value)
+
+  def add_edge(self, from_node, to_node, distance):
+    self.neighbours[from_node].append(to_node)
+    self.neighbours[to_node].append(from_node)
+    self.distances[(from_node, to_node)] = distance
+    self.distances[(to_node, from_node)] = distance
+
+  def print_graph(self):
+    print("set of nodes", self.nodes)
+    print("neighbours are", self.neighbours)
+    print("distance are", self.distances)
