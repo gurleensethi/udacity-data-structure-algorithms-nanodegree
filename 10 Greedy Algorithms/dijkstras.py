265b0b5d3014e7d71d60dc783811d617000fb212
Gurleen Sethi <sarusethi@rocketmail.com>
2020-06-20 18:08:35 -0400

    Dijkstras

--- 10 Greedy Algorithms/dijkstras.py
+++ 10 Greedy Algorithms/dijkstras.py
@@ -1,4 +1,6 @@
 from collections import defaultdict
+import sys
+
 class Graph:
   def __init__(self):
     self.nodes = set()
@@ -18,3 +20,52 @@ class Graph:
     print("set of nodes", self.nodes)
     print("neighbours are", self.neighbours)
     print("distance are", self.distances)
+
+def dijkstra(graph, source):
+  unvisited = set(graph.nodes)
+  result = {}
+  path = {}
+
+  for node in unvisited:
+    result[node] = sys.maxsize
+  
+  result[source] = 0
+
+  while unvisited:
+    min_node = None
+
+    for node in unvisited:
+      if min_node == None:
+        min_node = node
+      elif result[node] < result[min_node]:
+        min_node = node
+    
+    for neighbour in graph.neighbours[min_node]:
+      if neighbour in unvisited:
+        distance = result[min_node] + graph.distances[(min_node, neighbour)]
+
+        if neighbour not in result or distance < result[neighbour]:
+          print(min_node, "setting distance for", neighbour, "which is", distance)
+          result[neighbour] = distance
+          path[neighbour] = min_node
+    
+    unvisited.remove(min_node)
+
+    if min_node == None:
+      break
+  
+  return result
+
+testGraph = Graph()
+for node in ['A', 'B', 'C', 'D', 'E']:
+    testGraph.add_node(node)
+
+testGraph.add_edge('A','B',3)
+testGraph.add_edge('A','D',2)
+testGraph.add_edge('B','D',4)
+testGraph.add_edge('B','E',6)
+testGraph.add_edge('B','C',1)
+testGraph.add_edge('C','E',2)
+testGraph.add_edge('E','D',1)
+
+print(dijkstra(testGraph, 'A'))
