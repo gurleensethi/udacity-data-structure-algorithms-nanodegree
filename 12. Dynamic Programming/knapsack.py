a4f4adcf3a215d9be09e09e4787ebee3aec0184c
Gurleen Sethi <sarusethi@rocketmail.com>
2020-06-28 07:28:37 -0400

    knapsack

--- 12. Dynamic Programming/knapsack.py
+++ 12. Dynamic Programming/knapsack.py
@@ -1,4 +1,4 @@
-def knapsack_max_value(knapsack_max_weight, items):
+def knapsack_max_value_recur(knapsack_max_weight, items):
   return knapsack_max_recursive(knapsack_max_weight, items, len(items) - 1)
 
 def knapsack_max_recursive(capacity, items, last_index):
@@ -13,4 +13,15 @@ def knapsack_max_recursive(capacity, items, last_index):
 
   return max(valueA, valueB)
 
-print(knapsack_max_value(10, [{ 'weight': 4, 'value': 3 }, { 'weight': 9, 'value': 1 }, { 'weight': 5, 'value': 6 }, { 'weight': 4, 'value': 3 }]))
+def knapsack_max_value(knapsack_max_weight, items):
+  lookup = [0 for x in range(knapsack_max_weight + 1)]
+
+  for item in items:
+    for capacity in reversed(range(knapsack_max_weight + 1)):          
+      if item['weight'] < capacity:
+        lookup[capacity] = max(lookup[capacity], lookup[capacity - item['weight']] + item['value'])
+
+  return lookup[-1]
+
+items = [{ 'weight': 4, 'value': 3 }, { 'weight': 9, 'value': 1 }, { 'weight': 5, 'value': 6 }, { 'weight': 4, 'value': 3 }]
+print(knapsack_max_value(10, items))
